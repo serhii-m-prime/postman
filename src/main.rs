f571f2817d5175d992512f0cceb27cf835fc711d
Serhii M <s.maxymenko@gmail.com>
2026-05-17 16:13:54 +0100

    setup dev env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+
+    let project_name = "Postman CLI";
+    let mut articles_processed = 0;
+
+    let feeds = vec!["dronedj.com", "dji.com", "techcrunch.com"];
+
+    println!("--- Запуск відладки проєкту {} ---", project_name);
+
+    for single_feed in feeds {
+        articles_processed += 1;
+        
+        println!("Парсимо джерело: {}, Оброблено статей: {}", single_feed, articles_processed);
+    }
+
+    println!("--- Роботу завершено. Разом оброблено: {} ---", articles_processed);
 }
